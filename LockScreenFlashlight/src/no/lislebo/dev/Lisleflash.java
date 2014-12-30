ec70876a1efc957cd4c7b5c2e49abc6320d7fbde
Even Lislebø <e.lislebo@gmail.com>
2014-12-30 15:33:30 +0100

    Flash toggle working, adding safety nets to flashOff()

--- LockScreenFlashlight/src/no/lislebo/dev/Lisleflash.java
+++ LockScreenFlashlight/src/no/lislebo/dev/Lisleflash.java
@@ -1,6 +1,7 @@
 package no.lislebo.dev;
 
 import java.io.IOException;
+import java.util.List;
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -64,23 +65,42 @@ public class Lisleflash extends Activity implements SurfaceHolder.Callback
     }
 
     public void flashOn() {
-        Toast.makeText(this, "Flash on", Toast.LENGTH_SHORT).show();
         if(camera == null) {
+            Toast.makeText(this, "Camera not found, wtf", Toast.LENGTH_SHORT).show();
             return;
         }
         flashOn = true;
         Parameters parameters = camera.getParameters();
-        parameters.setFlashMode(Parameters.FLASH_MODE_TORCH);
+        if(parameters == null){
+            Toast.makeText(this, "Can't access camera parameters", Toast.LENGTH_SHORT).show();
+        }
+        List<String> supportedModes = parameters.getSupportedFlashModes();
+        if(supportedModes == null){
+            Toast.makeText(this, "Can't access flash modes", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        String mode = parameters.getFlashMode();
+        if( !mode.equals( Parameters.FLASH_MODE_TORCH ) ){
+            if( supportedModes.contains( Parameters.FLASH_MODE_TORCH ) ){
+                parameters.setFlashMode(Parameters.FLASH_MODE_TORCH);
+                camera.setParameters(parameters);
+            } else {
+                Toast.makeText(this, "Torch mode not supported", Toast.LENGTH_SHORT);
+            }
+        }
     }
 
     public void flashOff() {
-        Toast.makeText(this, "Flash off", Toast.LENGTH_SHORT).show();
+        if(!flashOn){
+            return;
+        }
         if(camera == null) {
             return;
         }
         flashOn = false;
         Parameters parameters = camera.getParameters();
         parameters.setFlashMode(Parameters.FLASH_MODE_OFF);
+        camera.setParameters(parameters);
     }
 
     private void startPreview() {
