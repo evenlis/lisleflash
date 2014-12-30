88543fa6058f21bb7f602ad5f7a6489d0b0d0673
Even Lislebø <e.lislebo@gmail.com>
2014-12-30 06:56:57 +0100

    overridden methods surface{Created,Changed,Destroyed} empty for now

--- LockScreenFlashlight/src/no/lislebo/dev/Lisleflash.java
+++ LockScreenFlashlight/src/no/lislebo/dev/Lisleflash.java
@@ -4,16 +4,22 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.hardware.Camera.Parameters;
 import android.hardware.Camera;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.widget.Toast;
 
-public class Lisleflash extends Activity
+public class Lisleflash extends Activity implements SurfaceHolder.Callback
 {
-
-    static Camera camera = null;
+    private boolean lightOn;
+    private boolean previewOn;
+    private View button;
+    private SurfaceView surfaceView;
+    private SurfaceHolder surfaceHolder;
+    static Camera camera;
 
     /** Called when the activity is first created. */
     @Override
@@ -23,26 +29,36 @@ public class Lisleflash extends Activity
         setContentView(R.layout.main);
     }
 
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int heihgt) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+
+    }
+
     public void toggle(View view) {
         flashOn();
     }
 
     public void flashOn() {
-        boolean camera_available = getApplicationContext().getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA);
-        boolean flash_available = getApplicationContext().getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
-        Context context = getApplicationContext();
-        CharSequence text = camera_available ? "Camera_available" : "Camera_unavailable";
-        int duration = Toast.LENGTH_SHORT;
-        Toast toast = Toast.makeText(context, text, duration);
-        toast.show();
         Parameters params;
         try {
             camera = Camera.open();
             params = camera.getParameters();
             params.setFlashMode(Parameters.FLASH_MODE_TORCH);
         } catch(Exception e) {
-            text = e.toString();
-            toast = Toast.makeText(context, text, duration);
+            Context context = getApplicationContext();
+            CharSequence text = e.toString();
+            int duration = Toast.LENGTH_SHORT;
+            Toast toast = Toast.makeText(context, text, duration);
             toast.show();
             Log.d("Lisleflash",e.toString());
         }
